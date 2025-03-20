d1006e4e59f5ff0c2080aec5410a02314246e0db
PyWebSol <safephone1.12@gmail.com>
2025-03-20 12:01:13 +0300

    Implement server management UI and configuration saving functionality
    - Added a new `AddServerWindow` struct to manage the server addition interface.
    - Enhanced the `Main` struct to include a new field for `add_server_window`.
    - Implemented a method to save the server configuration to a JSON file.
    - Updated the UI to display servers in a collapsible header and allow adding new servers through a dedicated window.
    - Improved user interaction with context menus for server entries.
    
    This commit significantly enhances the application's server management capabilities.

--- src/data/structs.rs
+++ src/data/structs.rs
@@ -12,12 +12,37 @@ pub struct Server {
     pub port: u16,
     pub user: String,
     pub password: String,
+    pub service_database: String,
 }
 
 impl Config {
     pub fn new() -> Self {
         Self {
             servers: Vec::new()
+        }
+    }
+}
+
+pub struct AddServerWindow {
+    pub show: bool,
+    pub name_field: String,
+    pub ip_field: String,
+    pub port_field: String,
+    pub user_field: String,
+    pub password_field: String,
+    pub service_database_field: String,
+}
+
+impl AddServerWindow {
+    pub fn new() -> Self {
+        Self {
+            show: false,
+            name_field: String::new(),
+            ip_field: String::new(),
+            port_field: String::from("5432"),
+            user_field: String::new(),
+            password_field: String::new(),
+            service_database_field: String::from("postgres"),
         }
     }
 }
--- src/frames.rs
+++ src/frames.rs
@@ -2,13 +2,19 @@ use crate::data::*;
 
 use eframe::{egui, App};
 
-use egui::{CentralPanel, Image, SidePanel, TopBottomPanel, Widget};
+use egui::{
+    CentralPanel, Window, SidePanel,
+    Widget, Layout, Align,
+    TextEdit, Color32, Button,
+    CollapsingHeader, Id, Sense,
+    containers::popup
+};
 
-use tokio::fs;
 use std::fs as std_fs;
 
 pub struct Main {
-    pub config: structs::Config,
+    config: structs::Config,
+    add_server_window: structs::AddServerWindow,
 }
 
 impl Main {
@@ -17,6 +23,7 @@ impl Main {
         
         Self {
             config: structs::Config::new(),
+            add_server_window: structs::AddServerWindow::new(),
         }
     }
 
@@ -38,6 +45,16 @@ impl Main {
         let config: structs::Config = serde_json::from_str(&config_file).unwrap();
         self.config = config;
     }
+
+    fn save_config(&mut self) {
+        let config_dir = &dirs::config_dir().unwrap().join("rs-postgres");
+        let config_path = &config_dir.join("config.json");
+
+        std_fs::write(
+            config_path,
+            serde_json::to_string(&self.config).unwrap()
+        ).unwrap();
+    }
 }
 
 impl App for Main {
@@ -58,23 +75,97 @@ impl App for Main {
         });
 
         SidePanel::left("left_panel").show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                server_icon.ui(ui);
-                ui.label("Servers");
+            CollapsingHeader::new("Servers").show(ui, |ui| {
+                for server in &self.config.servers {
+                    ui.horizontal(|ui| {
+                        database_icon.clone().ui(ui);
+
+                        let id = Id::new(format!("server:{}:{}:{}", server.ip, server.port, server.user));
+                        let button = ui.add(Button::new(format!("{} ({}:{})", server.alias, server.ip, server.port)));
+                        
+                        let interact_response = ui.interact(
+                            button.rect,
+                            id,
+                            Sense::click(),
+                        );
+                        if interact_response.clicked() {
+                            println!("Server clicked");
+                            // TODO
+                        } else if interact_response.secondary_clicked() {
+                            ui.memory_mut(|mem| mem.open_popup(id));
+                        }
+
+                        if ui.memory(|mem| mem.is_popup_open(id)) {
+                            interact_response.context_menu(|ui| {
+                                ui.label("test");
+                            });
+                        }
+                    });
+                }
             });
 
-            for server in &self.config.servers {
-                ui.horizontal(|ui| {
-                    database_icon.clone().ui(ui);
-                    ui.label(
-                        format!("{} ({}:{})", server.alias, server.ip, server.port)
-                    );
+            if self.add_server_window.show {
+                Window::new("New server").show(ctx, |ui| {
+                    ui.horizontal(|ui| {
+                        ui.label("Name");
+                        ui.add(TextEdit::singleline(&mut self.add_server_window.name_field));
+                    });
+                    ui.horizontal(|ui| {
+                        ui.label("Server address");
+                        ui.add(TextEdit::singleline(&mut self.add_server_window.ip_field));
+                    });
+                    ui.horizontal(|ui| {
+                        ui.label("Port");
+                        let is_error: bool = self.add_server_window.port_field.parse::<u16>().is_err();
+                        let mut field = TextEdit::singleline(&mut self.add_server_window.port_field);
+                        if is_error {
+                            field = field.text_color(
+                                Color32::from_rgb(255, 0, 0)
+                            );
+                        }
+                        ui.add(field);
+                    });
+                    ui.horizontal(|ui| {
+                        ui.label("User");
+                        ui.add(TextEdit::singleline(&mut self.add_server_window.user_field));
+                    });
+                    ui.horizontal(|ui| {
+                        ui.label("Password");
+                        ui.add(TextEdit::singleline(&mut self.add_server_window.password_field));
+                    });
+                    ui.horizontal(|ui| {
+                        ui.label("Service DB");
+                        ui.add(TextEdit::singleline(&mut self.add_server_window.service_database_field));
+                    });
+
+                    ui.with_layout(Layout::bottom_up(Align::RIGHT), |ui| {
+                        ui.horizontal(|ui| {
+                            let is_port_error: bool = self.add_server_window.port_field.parse::<u16>().is_err();
+
+                            if ui.add_enabled(!is_port_error, Button::new("Save")).clicked() {
+                                let server = structs::Server {
+                                    alias: self.add_server_window.name_field.clone(),
+                                    ip: self.add_server_window.ip_field.clone(),
+                                    port: self.add_server_window.port_field.clone().parse::<u16>().unwrap(),
+                                    user: self.add_server_window.user_field.clone(),
+                                    password: self.add_server_window.password_field.clone(),
+                                    service_database: self.add_server_window.service_database_field.clone(),
+                                };
+                                self.config.servers.push(server);
+                                self.save_config();
+
+                                self.add_server_window = structs::AddServerWindow::new();
+                            }
+                            if ui.button("Back").clicked() {
+                                self.add_server_window = structs::AddServerWindow::new();
+                            }
+                        });
+                    });
                 });
             }
 
             if ui.button("Add server").clicked() {
-                println!("Clicked button.");
-                // TODO
+                self.add_server_window.show = true;
             }
         });
     }
